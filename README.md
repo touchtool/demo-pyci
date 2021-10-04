16196eed4e225cc7d6972f7f57df5bc42549e454
Kittison <kittison.j@ku.th>
2021-10-04 15:05:10 +0700

    add markdown tranvis

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![Build Status](https://travis-ci.com/touchtool/demo-pyci.svg?branch=master)](https://travis-ci.com/touchtool/demo-pyci)
 Continuous Integration Demo
 ============================
 
--- stats_test.py
+++ stats_test.py
@@ -6,9 +6,9 @@ class StatsTest(TestCase):
 
     def test_variance_typical_values(self):
         """variance of some typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
+        self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
 
     def test_variance_single_value_is_zero(self):
         """variance of a singleton list should be zero"""
@@ -19,6 +19,7 @@ class StatsTest(TestCase):
         """variance of an empty list should raise an exception"""
         with self.assertRaises(ValueError):
             var = variance([])
+
 
 if __name__ == '__main__':
     import unittest
